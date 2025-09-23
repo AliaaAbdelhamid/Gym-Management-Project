aacaaf3b44b5c2787b306823c1bd57e7ce997d1c
Aliaa Tarek <Aliaa Tarek@LAPTOP-LJC895S6>
2025-09-23 15:58:32 +0300

    Start

--- GymManagement.DAL/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GymManagement.DAL
-{
-	public class Class1
-	{
-
-	}
-}
---
+++ GymManagementDAL/Repositories/Interfaces/IMemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+	internal interface IMemberRepository
+	{
+		MemberEntity? GetByIdAsync(int id);
+		IEnumerable<MemberEntity> GetAllAsync();
+		int Add(MemberEntity member);
+		int Update(MemberEntity member);
+		int Delete(int id);
+	}
+}
